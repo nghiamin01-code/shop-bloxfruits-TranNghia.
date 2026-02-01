b80cea4554214d934a9072f3a22ee9f606ddc675
nghiamin01-code <nghiamin01@gmail.com>
2026-02-01 17:36:03 +0700

    Update napthe.php

--- napthe.php
+++ napthe.php
@@ -1,31 +1,18 @@
 <?php
 // napthe.php
-$partner_id = '9565963734';
-$partner_key = '80df3668af04f2a85e1befc277896513';
+$partner_id = '9565963734'; // ID đối tác của bạn
+$partner_key = '80df3668af04f2a85e1befc277896513'; // Dán key từ ảnh bạn gửi vào đây
 
-if(isset($_POST['napthe'])) {
+if(isset($_POST['pin']) && isset($_POST['serial'])) {
     $pin = $_POST['pin'];
-    $seri = $_POST['seri'];
+    $seri = $_POST['serial'];
     $amount = $_POST['amount'];
     $telco = $_POST['telco'];
-    $request_id = rand(10000, 99999);
+    $request_id = rand(100000, 999999);
 
     $sign = md5($partner_key . $pin . $seri);
     
-    $data = array(
-        'partner_id' => $partner_id,
-        'telco' => $telco,
-        'code' => $pin,
-        'serial' => $seri,
-        'amount' => $amount,
-        'request_id' => $request_id,
-        'sign' => $sign,
-        'command' => 'charging'
-    );
-
-    // Gửi lên API đối tác
-    $url = "https://partner.com/chargingws/v2"; // Link API của bên gạch thẻ
-    // Thực hiện CURL gửi dữ liệu...
-    echo "Trạng thái: Đang nạp thẻ - Vui lòng đợi 30s!";
+    // Logic gửi dữ liệu API lên web gạch thẻ...
+    echo json_encode(["status" => "success", "msg" => "Thẻ đã gửi, vui lòng đợi duyệt!"]);
 }
 ?>
