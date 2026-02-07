48297aa35421cd58cf09d54e0da8ecdee6ce1719
nghiamin01-code <nghiamin01@gmail.com>
2026-02-07 18:12:25 +0700

    Update napthe.php

--- api/napthe.php
+++ api/napthe.php
@@ -1,16 +1,15 @@
 <?php
-// Thay API Key của Nghĩa vào đây (Lấy ở web gạch thẻ)
-$api_key = '80df3668af04f2a85e1befc277896513'; 
+$partner_id = '9565963734'; // Lấy ở web gạch thẻ
+$partner_key = '80df3668af04f2a85e1befc277896513'; 
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $type = $_POST['type'];
     $amount = $_POST['amount'];
     $serial = $_POST['serial'];
     $pin = $_POST['pin'];
-    $request_id = rand(100000, 999999); // Mã đơn hàng ngẫu nhiên
+    $request_id = rand(100000, 999999);
 
-    // Gửi dữ liệu sang web gạch thẻ (Ví dụ: doithe1s, thesieure...)
-    $url = "https://doithe1s.vn/api/card-auto?type=$type&amount=$amount&pin=$pin&serial=$serial&request_id=$request_id&partner_id=$api_key";
+    $url = "https://doithe1s.vn/api/card-auto?type=$type&amount=$amount&pin=$pin&serial=$serial&request_id=$request_id&partner_id=$partner_id&partner_key=$partner_key";
 
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
@@ -18,6 +17,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $response = curl_exec($ch);
     curl_close($ch);
 
-    echo $response; // Trả kết quả về cho web
+    echo $response;
 }
 ?>
